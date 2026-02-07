0a99e16b0f18ca1d8ee1e1f0399936787088cfb9
shubham chauhan <thakurshubhamchauhan99@gmail.com>
2026-02-07 04:53:46 +0000

    connect  to mongo atlas

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+
+export const DB_NAME = "shubham_Youtube_dp"
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import dotenv from "dotenv"
+import connectDB  from "./db/index.js";
+dotenv.config({
+  path: "./env"
+})
+
+connectDB();
+
+
+
+
+
+
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+
+
+/*
+import express from "express";
+const app = express();
+( async () =>{
+   try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on("error", (error) => {
+      console.log("ERROR", (error)=> {
+        console.log("error:", error);
+        throw error;
+        
+      })
+      app.listen(process.env.PORT, ()=> {
+        console.log(`APP is listening on port${process.env.PORT}`);
+        
+      })
+    })
+   }catch(error){
+    console.error("ERROR:", error)
+    throw error;
+   }
+})();
+*/
